ada7a7ad9780ba7bb2b42d1b355cf432c3aa5cb1
nischayydv <nischay419@gmail.com>
2025-07-06 06:28:22 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -1,68 +1,74 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 import os
-import re
-from os import environ, getenv
+from os import environ
+import logging
 
-id_pattern = re.compile(r'^.\d+$')
-
-def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
-    else:
-        return default
+logging.basicConfig(
+    format='%(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('log.txt'),
+              logging.StreamHandler()],
+    level=logging.INFO
+)
 
 class Config(object):
-    # Bot Information 
-    TECH_VJ_BOT_TOKEN = "8021806435:AAFGhQDVA3OXJMmtM74qSSFSQyeeFNRiw2A"
-    TECH_VJ_BOT_USERNAME = "Url_Uploader_NY_Bot"  # Bot username without @.
 
-    # The Telegram API things
-    TECH_VJ_API_ID = 24720215
-    TECH_VJ_API_HASH = "c0d3395590fecba19985f95d6300785e"
+    # Required Telegram Bot Credentials
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8021806435:AAFGhQDVA3OXJMmtM74qSSFSQyeeFNRiw2A")
+    API_ID = int(os.environ.get("API_ID", 24720215))
+    API_HASH = os.environ.get("API_HASH", "c0d3395590fecba19985f95d6300785e")
 
-    # The download location, where the HTTP Server runs
-    TECH_VJ_DOWNLOAD_LOCATION = "./DOWNLOADS"
+    # Bot username (optional)
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Url_Uploader_NY_Bot")
+    SESSION_NAME = "UploaderXNTBot"
+    SESSION_STR = ""
 
-    # Telegram maximum file upload size
-    TECH_VJ_MAX_FILE_SIZE = 2097152000
-    TECH_VJ_TG_MAX_FILE_SIZE = 2097152000
-    TECH_VJ_FREE_USER_MAX_FILE_SIZE = 2097152000
+    # Download Path
+    DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS")
 
-    # Chunk size that should be used with requests
-    TECH_VJ_CHUNK_SIZE = 128
+    # File Limits
+    MAX_FILE_SIZE = 2194304000
+    TG_MAX_FILE_SIZE = 2194304000
+    FREE_USER_MAX_FILE_SIZE = 2194304000
+    TG_MIN_FILE_SIZE = 2194304000
 
-    # Default thumbnail to be used in the videos
-    TECH_VJ_HTTP_PROXY = ""
+    # Chunk Size
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
 
-    # Maximum message length in Telegram
-    TECH_VJ_MAX_MESSAGE_LENGTH = 4096
+    # Thumbnail
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
 
-    # Set timeout for subprocess
-    TECH_VJ_PROCESS_MAX_TIMEOUT = 0
+    # Watermark
+    DEF_WATER_MARK_FILE = os.environ.get("DEF_WATER_MARK_FILE", "@UploaderXNTBot")
 
-    # Your telegram account ID
-    TECH_VJ_OWNER_ID = 7910994767
-    TECH_VJ_SESSION_NAME = "Url_Uploader_NY_Bot"
+    # Proxy and Timeouts
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    PROCESS_MAX_TIMEOUT = 3600
 
-    # Database URI (mongodb)
-    TECH_VJ_DATABASE_URL = "mongodb+srv://Nischay999:Nischay999@cluster0.5kufo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    
-    TECH_VJ_MAX_RESULTS = "50"
+    # Logging
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002732334186"))
+    LOGGER = logging
 
-    # Channel Information
-    TECH_VJ_LOG_CHANNEL = -1002732334186  # Your log channel ID and make bot admin in log channel with full rights
+    # Admins & Banned
+    OWNER_ID = int(os.environ.get("OWNER_ID", "7910994767"))
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
 
-    # If you want force subscribe then give your channel ID below else leave blank
-    TECH_VJ_UPDATE_CHANNEL = -1002465691872  # Your update channel ID and make bot admin in update channel with full rights
-    TECH_VJ_UPDATES_CHANNEL = -1002465691872
+    # Database
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Nischay999:Nischay999@cluster0.5kufo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-    # Url Shortener Information
-    TECH_VJ = False  # Set False if you want shortlink off else True
-    TECH_VJ_URL = ""  # Your shortlink URL domain or URL without https://
-    TECH_VJ_API = ""  # Your URL shortener API
-    TECH_VJ_TUTORIAL = "https://t.me/How_To_Open_Linkl"  # Tutorial link
+    # Channels
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002465691872")
+
+    # Message Settings
+    MAX_MESSAGE_LENGTH = 4096
+
+    # Shortlink
+    SHORT_DOMAIN = environ.get("SHORT_DOMAIN", "")
+    SHORT_API = environ.get("SHORT_API", "")
+    TRUE_OR_FALSE = os.environ.get("TRUE_OR_FALSE", "").lower() == "true"
+
+    # Tutorial / Help Link
+    VERIFICATION = os.environ.get("VERIFICATION", "")
+    TUTORIAL_LINK = os.environ.get("TUTORIAL_LINK", "https://t.me/How_To_Open_Linkl")
+
+    # Other Custom Values
+    ADL_BOT_RQ = {}
+    MAX_RESULTS = int(os.environ.get("MAX_RESULTS", "50"))

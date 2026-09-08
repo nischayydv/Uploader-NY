79bb420dc7dae622f357142c0eab4492156c286d
nischayydv <nischay419@gmail.com>
2026-09-08 20:05:25 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -12,7 +12,7 @@ logging.basicConfig(
 class Config(object):
 
     # Required Telegram Bot Credentials
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8021806435:AAFGhQDVA3OXJMmtM74qSSFSQyeeFNRiw2A")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8021806435:AAHng_yrAPjYAiA4x0HGcaWW0uW5DxK-v84")
     API_ID = int(os.environ.get("API_ID", 24720215))
     API_HASH = os.environ.get("API_HASH", "c0d3395590fecba19985f95d6300785e")
 
